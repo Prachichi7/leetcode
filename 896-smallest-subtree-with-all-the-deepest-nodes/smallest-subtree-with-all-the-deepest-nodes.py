35349ab6b23d56a629d14a1e51337c69e5cbe936
Prachi <143865589+Prachichi7@users.noreply.github.com>
2026-01-09 16:29:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (11.50%) - LeetSync

---
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(node: Optional[TreeNode]) -> Tuple[Optional[TreeNode], int]:
+            if node is None:
+                return None, 0
+
+            left_subtree, left_depth = dfs(node.left)
+            right_subtree, right_depth = dfs(node.right)
+          
+            if left_depth > right_depth:
+                return left_subtree, left_depth + 1
+          
+            if left_depth < right_depth:
+                return right_subtree, right_depth + 1
+            return node, left_depth + 1
+    
+        return dfs(root)[0]
