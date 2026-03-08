603c4d68d846aac6d5282ee77a0ac22106995525
Prachi <143865589+Prachichi7@users.noreply.github.com>
2026-03-08 12:16:57 +0530

    Time: 4 ms (99.79%) | Memory: 46.2 MB (31.59%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x > 0 && x % 10 == 0)) {
+            return false;
+        }
+      
+        int reversehalf = 0;
+      
+        while (reversehalf < x) {
+            reversehalf = reversehalf * 10 + x % 10;
+            x /= 10;
+        }
+      
+        return x == reversehalf || x == reversehalf / 10;
+    }
+}
