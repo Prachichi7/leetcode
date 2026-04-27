bb5a7585415543fa25d03db99fe711fd7882000c
Prachi <143865589+Prachichi7@users.noreply.github.com>
2026-04-27 11:25:42 +0530

    Time: 577 ms (9.70%) | Memory: 52.4 MB (33.58%) - LeetSync

---
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,67 @@
+class Solution:
+    def hasValidPath(self, grid: List[List[int]]) -> bool:
+        rows, cols = len(grid), len(grid[0])
+      
+        parent = list(range(rows * cols))
+      
+        def find(node: int) -> int:
+            if parent[node] != node:
+                parent[node] = find(parent[node])
+            return parent[node]
+      
+        def connect_left(row: int, col: int) -> None:
+            
+            if col > 0 and grid[row][col - 1] in (1, 4, 6):
+                current_root = find(row * cols + col)
+                left_root = find(row * cols + col - 1)
+                parent[current_root] = left_root
+      
+        def connect_right(row: int, col: int) -> None:
+           
+            if col < cols - 1 and grid[row][col + 1] in (1, 3, 5):
+                current_root = find(row * cols + col)
+                right_root = find(row * cols + col + 1)
+                parent[current_root] = right_root
+      
+        def connect_up(row: int, col: int) -> None:
+           
+            if row > 0 and grid[row - 1][col] in (2, 3, 4):
+                current_root = find(row * cols + col)
+                up_root = find((row - 1) * cols + col)
+                parent[current_root] = up_root
+      
+        def connect_down(row: int, col: int) -> None:
+            
+            if row < rows - 1 and grid[row + 1][col] in (2, 5, 6):
+                current_root = find(row * cols + col)
+                down_root = find((row + 1) * cols + col)
+                parent[current_root] = down_root
+      
+        for row in range(rows):
+            for col in range(cols):
+                street_type = grid[row][col]
+              
+             
+                if street_type == 1:
+                    connect_left(row, col)
+                    connect_right(row, col)
+                elif street_type == 2:  
+                    connect_up(row, col)
+                    connect_down(row, col)
+                elif street_type == 3:  
+                    connect_left(row, col)
+                    connect_down(row, col)
+                elif street_type == 4: 
+                    connect_right(row, col)
+                    connect_down(row, col)
+                elif street_type == 5:  
+                    connect_left(row, col)
+                    connect_up(row, col)
+                else:  
+                    connect_right(row, col)
+                    connect_up(row, col)
+      
+        start_root = find(0)
+        end_root = find(rows * cols - 1)
+        return start_root == end_root
+        
